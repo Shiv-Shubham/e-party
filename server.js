bfa0895ba62c67a0ec3dc53c17c495213a63fd9b
shivshubham <shubhamraj7239772@gmail.com>
2025-12-02 23:38:49 +0530

    changed the configuration of mongodb

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Mongoose Connected'))
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error(err));
+
 
 // Message schema
 const messageSchema = new mongoose.Schema({
